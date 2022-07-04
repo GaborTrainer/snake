1c8c14869bf0709b5338fcc1ff3884803fa84015
Gabor Nagy <szemelyiedzo29@gmail.com>
2022-07-04 09:59:38 +0200

    highScore

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
 const { table } = require('table');
+const menuData = require('./menuData.json');
+const highScoreData = require('./highScore.json');
 
 const mainMenu = () => {
-  const menuData = [
-    ['new game'],
-    ['settings'],
-    ['high scores'],
-    ['about the game'],
-    ['version'],
-    ['exit']
-  ];
-
-  return table(menuData);
+  return table(menuData, {
+    columnDefault: {
+      alignment: 'center',
+    }
+  });
 }
+
 console.log(mainMenu());
 
 const highScore = () => {
+  return table(highScoreData, {
+    columnDefault: {
+      alignment: 'center',
+    }
+  });
+}
 
-  const highScoreData = [
-    ['user', 'high Score'],
-    ['', 1],
-    ['', 2],
-    ['', 3],
-    ['', 4],
-    ['', 5]
-  ];
-
-  return table(highScoreData);
-  }
 console.log(highScore());
+
+const gameTitle = () => {
+  const text = 'snake game by Gabor Nagy';
+  const textLength = text.length;
+  const terminalLength = 80; //process.stdout.columns;
+  console.log(terminalLength);
+  const offset = (terminalLength - textLength) / 2;
+  let offsetString = '';
+  for (let i = 0; i < offset; i++) {
+    offsetString = ' ' + offsetString;
+  }
+  const resultText = offsetString + text;
+  console.log(resultText);
+};
+gameTitle();
